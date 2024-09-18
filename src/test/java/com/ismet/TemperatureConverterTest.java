5be7ade6b4294a17848ba8624674f9a73750f387
kismet <kismet1985@hotmail.fi>
2024-09-18 13:08:32 +0300

    ff

--- src/test/java/com/ismet/TemperatureConverterTest.java
+++ src/test/java/com/ismet/TemperatureConverterTest.java
@@ -1,9 +1,9 @@
 package com.ismet;
 
-import static org.junit.Assert.*;
-import org.junit.Before;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class TemperatureConverterTest {
 
