720b6d7376493bd97f334caf25e31276cc72a921
kismet <kismet1985@hotmail.fi>
2024-09-29 22:28:16 +0300

    tehtävä6

--- src/main/java/com/ismet/TemperatureConverter.java
+++ src/main/java/com/ismet/TemperatureConverter.java
@@ -3,6 +3,18 @@ package com.ismet;
 
 public class TemperatureConverter {
 
+    public static void main(String[] args) {
+        TemperatureConverter converter = new TemperatureConverter();
+
+        // Example usage
+        double fahrenheit = 98.6;
+        double celsius = converter.fahrenheitToCelsius(fahrenheit);
+        System.out.println(fahrenheit + " Fahrenheit is " + celsius + " Celsius");
+
+        double kelvin = 300;
+        double celsiusFromKelvin = kelvinToCel(kelvin);
+        System.out.println(kelvin + " Kelvin is " + celsiusFromKelvin + " Celsius");
+    }
     public double fahrenheitToCelsius(double fahrenheit) {
         return (fahrenheit - 32) * 5 / 9;
     }
